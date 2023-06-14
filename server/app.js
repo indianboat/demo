aa974a3ba7ff5a06fb36cd6b6a919c263592b4ee
unknown <iampankaj852@gmail.com>
2023-06-14 19:53:25 +0530

    sever bug fixed

--- server/app.js
+++ server/app.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const router = require('./router/route'); // require router
+const router = require('./router/route');
 const app = express();
 const port = process.env.PORT || 5000;
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-// const cors = require('cors');
 
 require('./db/db');
-
-app.use(router); // using router
-
-// app.use(cors({ origin: "*" }));
+app.use(router); 
 
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
