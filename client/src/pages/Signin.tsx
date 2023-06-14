d41d48f3aee64c27338e52de8c84e5c7331a4bb1
unknown <iampankaj852@gmail.com>
2023-06-14 20:51:05 +0530

    checking error5

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -24,52 +24,37 @@ export default function Signin() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
 
-    try {
-      const res = await fetch('https://mydemy.onrender.com/signin', {
-        method: 'POST',
-        headers: {
-          "Content-Type": 'application/json',
-        },
-        body: JSON.stringify(data)
-      })
-      const result = await res.json();
-      console.log(result);
+      try {
 
-    } catch (error) {
-      console.log('Error: ', error);
-    }
+        const res = await fetch('https://mydemy.onrender.com/signin', {
+          method: 'POST',
+          headers: {
+            "Content-Type": 'application/json',
+          },
+          body: JSON.stringify(data)
+        })
+        const result = await res.json();
 
-    //   try {
-
-    //     const res = await fetch('/signin', {
-    //       method: 'POST',
-    //       headers: {
-    //         "Content-Type": 'application/json',
-    //       },
-    //       body: JSON.stringify(data)
-    //     })
-    //     const result = await res.json();
-
-    //     console.log(result);
+        console.log(result);
 
 
-    //     if (result.authenticated === false) {
-    //       alert('invalid credentials!');
-    //     }
-    //     else if (result.authenticated === true) {
+        if (result.authenticated === false) {
+          alert('invalid credentials!');
+        }
+        else if (result.authenticated === true) {
 
-    //       setCookie("jwt_token", result.token);
-    //       alert('login success!');
-    //       dispatch(updateuser((cookies && cookies.jwt_token && jwt(cookies.jwt_token)) ? jwt(cookies.jwt_token) : ''))
-    //       console.log('after sign in  dispatch ', cookies && cookies.jwt_token && jwt(cookies.jwt_token) ? jwt(cookies.jwt_token) : 'not value till yet')
-    //       navigate('/');
-    //       window.location.reload();
-    //       // console.log('signin')
-    //     }
+          setCookie("jwt_token", result.token);
+          alert('login success!');
+          dispatch(updateuser((cookies && cookies.jwt_token && jwt(cookies.jwt_token)) ? jwt(cookies.jwt_token) : ''))
+          console.log('after sign in  dispatch ', cookies && cookies.jwt_token && jwt(cookies.jwt_token) ? jwt(cookies.jwt_token) : 'not value till yet')
+          navigate('/');
+          window.location.reload();
+          // console.log('signin')
+        }
 
-    //   } catch (error) {
-    //     console.log('error,not fetched!', error);
-    //   }
+      } catch (error) {
+        console.log('error,not fetched!', error);
+      }
 
   }
 
